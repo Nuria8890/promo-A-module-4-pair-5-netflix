5f781462e03204ff249485bbb2706d23b938a0af
Nuria Campo <nuria.campo7@gmail.com>
2024-12-12 11:10:49 +0100

    ya se veee YUUHUUUU

--- web/src/components/App.jsx
+++ web/src/components/App.jsx
@@ -39,7 +39,7 @@ const App = () => {
       sort: allMoviesOptionSort,
     };
     apiMovies.getMoviesFromApi(params).then((response) => {
-      setAppMovies(response.result);
+      setAppMovies(response);
     });
   }, [allMoviesOptionGenre, allMoviesOptionSort]);
 
--- web/src/services/api-movies.jsx
+++ web/src/services/api-movies.jsx
@@ -1,19 +1,18 @@
 // login
 
 const getMoviesFromApi = () => {
-  console.log('Se están pidiendo las películas de la app');
+  console.log("Se están pidiendo las películas de la app");
   // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
-  return fetch('http://localhost:4000/movies')
-    .then(response => response.json())
-    .then(data => {
-      console.log("data", data)
-      return data;
+  return fetch("http://localhost:4000/movies")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("data.message", data.message);
+      return data.message;
     });
-
 };
 
 const objToExport = {
-  getMoviesFromApi: getMoviesFromApi
+  getMoviesFromApi: getMoviesFromApi,
 };
 
 export default objToExport;
